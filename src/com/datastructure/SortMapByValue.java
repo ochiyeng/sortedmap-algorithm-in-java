7a3d00e2a4d77924bf59f038d3964dfc0d79c325
peter Grahams <deanwellsg@gmail.com>
2020-03-28 12:16:42 +0300

    Main class and methods added

--- src/com/datastructure/SortMapByValue.java
+++ src/com/datastructure/SortMapByValue.java
@@ -1,21 +1,61 @@
 package com.datastructure;
 
-import java.util.HashMap;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.HashMap;
 
 public class SortMapByValue {
-    public static void main(String[] args){
-        Map initialMap = new HashMap<>();
-        initialMap.put("J", 10);
-        initialMap.put("A" , 5);
-        initialMap.put("V", 6);
-        initialMap.put("C", 20);
-        initialMap.put("O", 8);
-        initialMap.put("R", 99);
-        initialMap.put("E", 9);
 
-        System.out.println("initialMap:  " + initialMap);
-        Map sortedMap = SortMapByValue(initialMap);
-        System.out.println("sortedMap: " + sortedMap);
-    } 
+    public static void main(String[] args) {
+        Map<String, String> unsortMap = new HashMap<String, String>();
+        unsortMap.put("J", "j");
+        unsortMap.put("A", "a");
+        unsortMap.put("V", "v");
+        unsortMap.put("C", "c");
+        unsortMap.put("M", "m");
+        unsortMap.put("F", "f");
+        unsortMap.put("Y", "y");
+        unsortMap.put("T", "t");
+        unsortMap.put("E", "e");
+        unsortMap.put("B", "b");
+
+        System.out.println("UnsortMap: .......................................");
+        printMap(unsortMap);
+        System.out.println("\n=================================================");
+
+        System.out.println("\nSorted Map....................-> by (key)");
+        Map<String, String> treeMap = new TreeMap<String, String>(unsortMap);
+        printMap(treeMap);
+
+    }
+
+    /*
+    print a map
+     */
+    public static <K, V> void printMap(Map<K, V> map) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            System.out.println("Key:   " + entry.getKey() + " Value:   " + entry.getValue());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
